a3ec96f1f3d4413eb0a1ead96964b40441253a13
Alkın Çakıralar <a.cakiralar@gmail.com>
2022-04-26 18:41:23 +0300

    slider

---
+++ Sources/TGPSliderLibrary/TGPSliderLibrary.swift
@@ -0,0 +1,6 @@
+public struct TGPSliderLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/TGPSliderLibraryTests/TGPSliderLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TGPSliderLibrary
+
+final class TGPSliderLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TGPSliderLibrary().text, "Hello, World!")
+    }
+}
